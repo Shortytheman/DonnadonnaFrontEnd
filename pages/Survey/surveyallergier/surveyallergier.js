8836780c98494aec46afd05bdbccd5a2c49da8bb
Shortytheman <shor7ty@gmail.com>
2022-11-28 12:30:08 +0100

    checkbox er lækker

--- pages/Survey/surveyallergier/surveyallergier.js
+++ pages/Survey/surveyallergier/surveyallergier.js
@@ -4,4 +4,13 @@ import { progressBarColor } from "../surveyBar.js";
 export function initSurveyAllergier() {
   getPopover()
   progressBarColor("allergier")
+  document.getElementById("myCheck").onclick = removebox
+}
+
+function removebox() {
+  if (document.getElementById('myCheck').checked) {
+      document.getElementById('area').style.display = 'none';
+  } else {
+      document.getElementById('area').style.display = 'block';
+  }
 }
