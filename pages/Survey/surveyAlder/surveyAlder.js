88a8cbd2f6fe572f76be3136a970012e4eba510b
JohannesSloth <joha218s@stud.kea.dk>
2022-11-28 11:38:23 +0100

    begyndt at forbinde surveypages

--- pages/Survey/surveyAlder/surveyAlder.js
+++ pages/Survey/surveyAlder/surveyAlder.js
@@ -1,7 +1,15 @@
 import {getPopover} from "../../../surveyUtils.js"
 import {progressBarColor} from "../surveyBar.js"
 
+
 export function initSurveyAlder() {
     getPopover()
     progressBarColor("alder")
+    document.getElementById("btn-surveyAlderNext").onclick = getSurveyAge
+}
+
+export let age;
+function getSurveyAge() {
+  age = document.getElementById("custAge").value
+  console.log(age);
 }
