fa04931a667b4d323990b4525865910cd9c2c300
JohannesSloth <joha218s@stud.kea.dk>
2022-11-28 11:58:16 +0100

    kan navigerer igennem de sider vi har so far nu

--- pages/Survey/surveyallergier/surveyallergier.js
+++ pages/Survey/surveyallergier/surveyallergier.js
@@ -4,4 +4,11 @@ import { progressBarColor } from "../surveyBar.js";
 export function initSurveyAllergier() {
   getPopover()
   progressBarColor("allergier")
+  document.getElementById("btn-surveyAllergierNext").onclick = getSurveyAllergies
+}
+
+export let allergies;
+function getSurveyAllergies() {
+  allergies = document.getElementById("custAllergies").value
+  console.log(allergies);
 }
